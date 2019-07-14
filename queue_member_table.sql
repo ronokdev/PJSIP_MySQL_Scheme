89666cb06b6cc504153d4b61d76f8516fa329dad
Farhan Fuad Ronok <25183566+rounook@users.noreply.github.com>
2019-07-14 18:14:14 +0600

    Create queue_member_table.sql

---
+++ queue_member_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE queue_member_table (
+  uniqueid INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  membername varchar(40),
+  queue_name varchar(128),
+  interface varchar(128),
+  penalty INT(11),
+  paused INT(11),
+  UNIQUE KEY queue_interface (queue_name, interface)
+);
