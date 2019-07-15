22d2352c7dc651d594b066c3188704d1abf5ba83
Farhan Fuad Ronok <25183566+rounook@users.noreply.github.com>
2019-07-15 13:10:24 +0600

    Create queue_log.sql

---
+++ queue_log.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `queue_log` (
+	`time` varchar (32) DEFAULT NULL,
+	`callid` char (64) DEFAULT NULL,
+	`queuename` char (64) DEFAULT NULL,
+	`agent` char (64) DEFAULT NULL,
+	`event` char (32) DEFAULT NULL,
+	`data` char (64) DEFAULT NULL,
+	`data1` char (64) DEFAULT NULL,
+	`data2` char (64) DEFAULT NULL,
+	`data3` char (64) DEFAULT NULL,
+	`data4` char (64) DEFAULT NULL,
+	`data5` char (64) DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
